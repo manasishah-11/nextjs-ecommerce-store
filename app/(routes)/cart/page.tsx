1f0c1e69bd575feb3f9975532a22bfc2cc37b5e4
Manasi Shah <manasishah@Manasis-MacBook-Air.local>
2023-10-10 22:54:21 +0530

    removed hydration check  from cart

--- app/(routes)/cart/page.tsx
+++ app/(routes)/cart/page.tsx
@@ -1,4 +1,4 @@
-// "use client";
+"use client";
 
 import { useEffect, useState } from "react";
 
@@ -12,7 +12,7 @@ export const revalidate = 0;
 
 const CartPage = () => {
   // const [isMounted, setIsMounted] = useState(false);
-  // const cart = useCart();
+  const cart = useCart();
 
   // useEffect(() => {
   //   setIsMounted(true);
@@ -29,14 +29,14 @@ const CartPage = () => {
           <h1 className="text-3xl font-bold text-black">Shopping Cart</h1>
           <div className="mt-12 lg:grid lg:grid-cols-12 lg:items-start gap-x-12">
             <div className="lg:col-span-7">
-              {/* {cart.items.length === 0 && (
+              {cart.items.length === 0 && (
                 <p className="text-neutral-500">No items added to cart.</p>
               )}
               <ul>
                 {cart.items.map((item) => (
                   <CartItem key={item.id} data={item} />
                 ))}
-              </ul> */}
+              </ul>
             </div>
             <Summary />
           </div>
