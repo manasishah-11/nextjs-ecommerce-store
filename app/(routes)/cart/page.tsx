c7306ce0c1d62c9d646fe108f9985e502fbb4007
Manasi Shah <manasishah@Manasis-MacBook-Air.local>
2023-10-10 23:25:16 +0530

    pre-render issue fixed

--- app/(routes)/cart/page.tsx
+++ app/(routes)/cart/page.tsx
@@ -1,20 +1,47 @@
 "use client";
-// import Container from "@/components/ui/container";
 
-// import CartLayout from "./components/cart-layout";
+import { useEffect, useState } from "react";
 
-// export const revalidate = 0;
+import Container from "@/components/ui/container";
+import useCart from "@/hooks/use-cart";
+
+import Summary from "./components/summary";
+import CartItem from "./components/cart-item";
+
+export const revalidate = 0;
 
 const CartPage = () => {
+  const [isMounted, setIsMounted] = useState(false);
+  const cart = useCart();
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) {
+    return null;
+  }
+
   return (
     <div className="bg-white">
-      Cart
-      {/* <Container>
+      <Container>
         <div className="px-4 py-16 sm:px-6 lg:px-8">
           <h1 className="text-3xl font-bold text-black">Shopping Cart</h1>
-          <CartLayout />
+          <div className="mt-12 lg:grid lg:grid-cols-12 lg:items-start gap-x-12">
+            <div className="lg:col-span-7">
+              {cart.items.length === 0 && (
+                <p className="text-neutral-500">No items added to cart.</p>
+              )}
+              <ul>
+                {cart.items.map((item) => (
+                  <CartItem key={item.id} data={item} />
+                ))}
+              </ul>
+            </div>
+            <Summary />
+          </div>
         </div>
-      </Container> */}
+      </Container>
     </div>
   );
 };
