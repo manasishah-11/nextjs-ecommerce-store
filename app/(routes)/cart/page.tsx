07888d685cdf22ae310f05009850b63711db9c21
Manasi Shah <manasishah@Manasis-MacBook-Air.local>
2023-10-10 23:18:49 +0530

    trying use client on cart

--- app/(routes)/cart/page.tsx
+++ app/(routes)/cart/page.tsx
@@ -1,21 +1,22 @@
-import Container from "@/components/ui/container";
+"use client";
+// import Container from "@/components/ui/container";
 
-import CartLayout from "./components/cart-layout";
+// import CartLayout from "./components/cart-layout";
 
-export const revalidate = 0;
+// export const revalidate = 0;
 
 const CartPage = () => {
   return (
     <div className="bg-white">
-      <Container>
+      Cart
+      {/* <Container>
         <div className="px-4 py-16 sm:px-6 lg:px-8">
           <h1 className="text-3xl font-bold text-black">Shopping Cart</h1>
           <CartLayout />
         </div>
-      </Container>
+      </Container> */}
     </div>
   );
-  return null;
 };
 
 export default CartPage;
